5553f9b5856fa5e19908e2e81ad6c183b9270077
thinhvoxuan <thinhvoxuan@gmail.com>
2016-03-13 12:32:56 +0700

    note app with navigating feature

--- notes-app/www/js/app.js
+++ notes-app/www/js/app.js
@@ -3,18 +3,71 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic', 'mynotes.notestores']);
 
-.run(function($ionicPlatform) {
+app.controller('ListCtrl', function($scope, $state, NoteStore){
+  $scope.reordering = false;
+  $scope.notes = NoteStore.list();
+  $scope.addNewNote = function(){
+    $state.go('add');
+  }
+  $scope.delete = function(noteId){
+    NoteStore.delete(noteId);
+  }
+  $scope.move = function(note, fromIndex, toIndex){
+    NoteStore.move(note, fromIndex, toIndex)
+  }
+  $scope.toogleReOrder = function(){
+    $scope.reordering = !$scope.reordering;
+  }
+})
+
+app.controller('EditCtrl', function($scope, $state, NoteStore){
+  $scope.note = angular.copy(NoteStore.get($state.params.nodeId));
+  $scope.saveNote = function(){
+    NoteStore.update($scope.note);
+    $state.go('list');
+  }
+});
+
+app.controller('AddCtrl', function($scope, $state, NoteStore){
+  $scope.note = {
+    id: (new Date()).getMilliseconds(),
+    title: '',
+    description: ''
+  };
+  $scope.saveNote = function(){
+    NoteStore.add($scope.note);
+    $state.go('list');
+  }
+});
+
+app.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('list',{
+      url: '/list',
+      templateUrl: 'templates/list.html',
+      controller: 'AddCtrl'
+    });
+
+    $stateProvider.state('edit', {
+      url: '/edit/:nodeId',
+      templateUrl: 'templates/edit.html',
+      controller: 'EditCtrl'
+    })
+
+    $stateProvider.state('add', {
+      url: '/add',
+      templateUrl: 'templates/edit.html',
+      controller: 'AddCtrl'
+    })
+
+    $urlRouterProvider.otherwise('/list');
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
